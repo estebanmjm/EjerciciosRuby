788cc5ad82b4a9b0e9e15334405667d6a69efbf0
Esteban Jiménez <mateojm77@gmail.com>
2016-02-19 11:33:08 -0500

    avance de retos

--- factprim.rb
+++ factprim.rb
@@ -1,12 +1,19 @@
-num=306
-i=2
+num=6008514
+numero=num
+
 for i in 2..num
-reinicio=i-(i-2)
-	if num%i==00
-		puts "#{num}|#{i}"
+	if num%i==0
+	    until num%i!=0 #este until con un incremento de cero, me asegura que se repita la iteración sin aumentar el i hasta que se cumpla la condicion
+	    puts "#{num}|#{i}"
 		num=num/i
-		
-		puts "#{num}|#{i}"
+		mfp=i #mayor factor primo hasta el momento
+			if num ==1 #este if con break es para evitar que se quede dividiendo a 1 por siempre y terminar el until..
+				break
+			end
+		i+=0
+		end	
 	end
-		i=2
 end
+puts "#{num}"
+puts "El mayor factor primo de #{numero} es: #{mfp}"
+		
---
+++ mult_peque.rb
@@ -0,0 +1,10 @@
+num=1
+div=1
+
+for num in 1..100000
+	
+		end	
+ 	end
+end
+		
+ 	
