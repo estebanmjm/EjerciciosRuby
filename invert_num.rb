7cd6a06c6dbb9dddf80cabb403f3a2e7e0e7fcea
Esteban Jiménez <mateojm77@gmail.com>
2016-02-24 22:08:49 -0500

    fin de los ejercicios de lógica

--- invert_num.rb
+++ invert_num.rb
@@ -2,16 +2,14 @@ puts "ingrese numero a invertir"
 numero = gets.chomp.to_i
 residuo = 0
 numero_inv = 0
-while numero >= 0
+while numero > 0 #si fuera >= 0, el algoritmo entra en un loop infinito
 	residuo = numero%10
-	numero = numero/10 - residuo
-	puts "#{numero}"
-	puts "#{residuo}"
-
+	numero = numero/10 - (residuo/10)
 	
-	numero_inv = (numero_inv+residuo)*10
+	numero_inv = numero_inv*10+residuo
 	residuo = 0
 
 end
-puts "#{numero_inv}"
+
+puts "El número invertido es: #{numero_inv}"
 
